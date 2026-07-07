f8ac8f9128c410570f7fdd5af3f423a1216ea5b3
Marc Matutes <matutesmarc@gmail.com>
2026-07-07 13:10:17 +0200

    Estructura inicial en C++ con CMake

---
+++ build_web/CMakeFiles/juego_raylib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for juego_raylib.
+# This may be replaced when dependencies are built.
---
+++ build_web/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marc/Documentos/ProyectosMios/Raylib/raylib-gamejam/raylib/src
+# Build directory: /home/marc/Documentos/ProyectosMios/Raylib/raylib-gamejam/raylib-gamejam-project/build_web/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
